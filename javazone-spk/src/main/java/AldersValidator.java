057979d36a108bc3d5b00371acfdf83cc81d4bd6
Johannes Brodwall <johannes@brodwall.com>
2010-09-01 19:01:17 +0200

    PERMASTER-JAVAZONE: Første utkast

---
+++ javazone-spk/src/main/java/AldersValidator.java
@@ -0,0 +1,14 @@
+import java.util.Calendar;
+
+
+public class AldersValidator {
+
+	public static boolean oppfyllerAldersgrense(Calendar foedt,
+			Calendar vilGaaAv) {
+		long alderIms = Calendar.getInstance().getTimeInMillis()-foedt.getTimeInMillis();
+		
+		
+		return alderIms / 1000L / 60L / 60L / 24L / 365L > 67L;
+	}
+
+}
---
+++ javazone-spk/src/test/java/SpkTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+
+public class SpkTest {
+	
+	@Test
+	public void skalVaereUnderAldersGrenseHvisYngreEnn67VedPensjoneringstidspunk() throws Exception {
+		Calendar foedt = Calendar.getInstance();
+		foedt.set(Calendar.YEAR, 1943);
+		foedt.set(Calendar.MONTH, Calendar.AUGUST);
+		foedt.set(Calendar.DAY_OF_MONTH, 26);
+		
+		
+		Calendar vilGaaAv = Calendar.getInstance();
+		vilGaaAv.set(Calendar.YEAR, 1943);
+		vilGaaAv.set(Calendar.MONTH, Calendar.NOVEMBER);
+		vilGaaAv.set(Calendar.DAY_OF_MONTH, 1);
+		
+		assertFalse(AldersValidator.oppfyllerAldersgrense(foedt, vilGaaAv));
+		
+	}
+
+	@Test
+	public void skalVaereOverAldersGrenseHvisEldreEnn67VedPensjoneringstidspunk() throws Exception {
+		Calendar foedt = Calendar.getInstance();
+		foedt.set(Calendar.YEAR, 1943);
+		foedt.set(Calendar.MONTH, Calendar.OCTOBER);
+		foedt.set(Calendar.DAY_OF_MONTH, 26);
+		
+		Calendar vilGaaAv = Calendar.getInstance();
+		vilGaaAv.set(Calendar.YEAR, 1943);
+		vilGaaAv.set(Calendar.MONTH, Calendar.NOVEMBER);
+		vilGaaAv.set(Calendar.DAY_OF_MONTH, 1);
+		
+		assertTrue(AldersValidator.oppfyllerAldersgrense(foedt, vilGaaAv));
+		
+	}
+}
