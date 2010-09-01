b06cb3a1e0767ed0be482e66d44c4dafc830ee54
Johannes Brodwall <johannes@brodwall.com>
2010-09-01 19:24:39 +0200

    Er det noen som leser disse?

--- javazone-spk/src/main/java/AldersValidator.java
+++ javazone-spk/src/main/java/AldersValidator.java
@@ -4,10 +4,18 @@ import java.util.Calendar;
 public class AldersValidator {
 	
 	public static boolean oppfyllerAldersgrense(Calendar foedt,
-			Calendar vilGaaAv) {
-		foedt.setTimeZone(vilGaaAv.getTimeZone());
+			Calendar vilGaaAv) throws Exception {
+		validerIkkeNull(foedt, vilGaaAv);
 		long alderIMillisekunder = Calendar.getInstance().getTimeInMillis()-foedt.getTimeInMillis();
 		return alderIMillisekunder / 1000L / 60L / 60L / 24L / 365L >= 67L;
+	}
+
+	private static void validerIkkeNull(Object... o) throws Exception {
+		for (Object object : o) {
+			if (object == null) {
+				throw new Exception("Input kan ikke være null");
+			}
+		}
 	}
 
 }
